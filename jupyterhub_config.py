198a19d9addd52bd32db25219f0450d54a2526f8
kitagawa-hr <kitagawa@cancerscan.jp>
2018-09-20 11:35:45 +0900

    modify to no-ssl

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -45,12 +45,13 @@ c.DockerSpawner.debug = True
 
 # User containers will access hub by container name on the Docker network
 c.JupyterHub.hub_ip = 'jupyterhub'
-c.JupyterHub.hub_port = 8080
+c.JupyterHub.hub_port = 8000
 
-# TLS config
-c.JupyterHub.port = 443
-c.JupyterHub.ssl_key = os.environ['SSL_KEY']
-c.JupyterHub.ssl_cert = os.environ['SSL_CERT']
+# # TLS config
+# c.JupyterHub.port = 443
+c.JupyterHub.port = 8080 
+# c.JupyterHub.ssl_key = os.environ['SSL_KEY']
+# c.JupyterHub.ssl_cert = os.environ['SSL_CERT']
 
 # Authenticate users with GitHub OAuth
 c.JupyterHub.authenticator_class = 'oauthenticator.GitHubOAuthenticator'
